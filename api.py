896904411e9a1fa3e74d457e8a3afcba1b1bfd0f
jerry960331 <jerry960331@gmail.com>
2021-08-26 01:59:58 +0800

    fix minor bugs

--- api.py
+++ api.py
@@ -71,7 +71,7 @@ def get_taiwan_outbreak_information(arg: str = ""):
 
     if not today.is_same_date and not isForce:
         text = "ERROR: 日期錯誤。本日衛福部新聞稿尚未更新？"
-        status = ERROR_NOT_SAME_DATE
+        status = ERROR_NOT_SAME_DATE + today.date
         return (text, ERROR_NOT_SAME_DATE, "")
 
     # 強制執行，且 today 無法取得時的處理
